e3d732546004bf576291acdf242863ad2e41c6b3
lee-ji-hoon <dlwlgns1240@naver.com>
2021-12-31 12:37:47 +0900

    100 - 알고리즘 시작

---
+++ src/com/company/Arithmetic/Arithmetic_10952.java
@@ -0,0 +1,25 @@
+package com.company.Arithmetic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Arithmetic_10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+            System.out.println(A+B);
+        }
+    }
+}

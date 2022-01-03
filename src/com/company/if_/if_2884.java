aa80018788169e67e08b7060f6aa4e55430529bc
lee-ji-hoon <dlwlgns1240@naver.com>
2022-01-03 21:31:25 +0900

    if문 끝

---
+++ src/com/company/if_/if_2884.java
@@ -0,0 +1,31 @@
+package com.company.if_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class if_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if (M < 45) { // M(분)이 45분 보다 작으면 H(시)에서 -1 해주고 M은 + 15 해주면 된다.
+            H--;
+            M = M + 15;
+            if (H < 0) { // H가 0이였을 경우 -1이 되므로 다시 24시간 계산식으로 바꿔줘야 한다.
+                H = H + 24;
+            }
+            System.out.println(H + " " + M);
+        }else{
+            M = M - 45; // 45보다 큰 경우 이므로 다른 것 신경쓸 필요 없이 M만 -45 해주면 된다.
+            System.out.println(H + " " + M);
+        }
+    }
+}
+
---
+++ src/com/company/if_/if_9498.java
@@ -0,0 +1,15 @@
+package com.company.if_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class if_9498 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int str = Integer.parseInt(br.readLine());
+
+        System.out.println((str >= 90) ? "A" : (str>= 80) ? "B" : (str>=70) ? "C" : (str>=60) ? "D" : "F");
+    }
+}

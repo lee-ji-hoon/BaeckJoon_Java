3e5a9e8a1e9f01f9853336e4205f806c0f6b5683
lee-ji-hoon <dlwlgns1240@naver.com>
2022-01-05 22:26:42 +0900

    for문 끝

---
+++ src/com/company/for_/for_10871.java
@@ -0,0 +1,32 @@
+package com.company.for_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class for_10871 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int val = Integer.parseInt(st.nextToken());
+
+            if (val < X) {
+                sb.append(val).append(" ");
+            }
+        }
+        System.out.println(sb);
+    }
+}

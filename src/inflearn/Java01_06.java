4dd4b36b7cb98c6ae8f5664add6085d55d37bed5
lee-ji-hoon <dlwlgns1240@naver.com>
2022-01-17 22:31:38 +0900

    인프런 01-06 풀이

---
+++ src/inflearn/Java01_06.java
@@ -0,0 +1,52 @@
+package inflearn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Java01_06 {
+
+/*    public String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            if (str.indexOf(str.charAt(i)) == i) sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Java01_06 T = new Java01_06();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println(T.solution(str));
+    }*/
+
+    public String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        Set<String> linkedHashSet = new LinkedHashSet<String>();
+        for (char x : str.toCharArray()) {
+            linkedHashSet.add(String.valueOf(x));
+        }
+
+        Iterator<String> linkedIter = linkedHashSet.iterator();
+
+        while(linkedIter.hasNext()) {
+            sb.append(linkedIter.next());
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Java01_06 T = new Java01_06();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(T.solution(str));
+    }
+}

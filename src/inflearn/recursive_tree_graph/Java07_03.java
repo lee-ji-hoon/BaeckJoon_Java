e6e8db5ac5c09f7c4e3a0187657a57dc7298f972
lee-ji-hoon <dlwlgns1240@naver.com>
2022-02-13 21:01:11 +0900

    잘못된 이름 변경

--- src/inflearn/recursive_tree_graph/Java07_03.java
+++ src/inflearn/recursive_tree_graph/Java07_03.java
@@ -3,27 +3,30 @@ package inflearn.recursive_tree_graph;
 import java.util.Scanner;
 
 /**
- * 1. DFS로 문제 풀어보기
- * 2. N == 0 일때 return
- * 3. DFS(N/2)
- * 4. N % 2 출력
- * DFS 호출 후 sout으로 출력하는 방법으로 해야 함
+ * 1. N 입력
+ * 2. N이 1로 입력된 경우는 return 1
+ * 3. 1이 아닌경우 return N * DFS(N-1);
+ *  3-1 N이 5라고 가정
+ *  3-2. 5 * D(4)
+ *  3-3. 5 * 4 * D(3)
+ *  3-4. 5 * 4 * 3 * D(2)
+ *  3-5. 5 * 4 * 3 * 2 * D(1)
+ *  3-6. 5 * 4 * 3 * 2 * 1
  */
-public class Java07_03 {
-    public void DFS(int N) {
 
-        if (N == 0) return;
-        else{
-            DFS(N / 2);
-            System.out.print(N % 2);
-        }
+public class Java07_03 {
+
+    public int DFS(int N) {
+        if (N == 1) return 1;
+        else return N * DFS(N - 1);
     }
 
     public static void main(String[] args) {
         Java07_03 T = new Java07_03();
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        T.DFS(N);
 
+        System.out.println(T.DFS(N));
     }
 }
--- src/inflearn/recursive_tree_graph/Java07_04.java
+++ src/inflearn/recursive_tree_graph/Java07_04.java
@@ -4,29 +4,36 @@ import java.util.Scanner;
 
 /**
  * 1. N 입력
- * 2. N이 1로 입력된 경우는 return 1
- * 3. 1이 아닌경우 return N * DFS(N-1);
- *  3-1 N이 5라고 가정
- *  3-2. 5 * D(4)
- *  3-3. 5 * 4 * D(3)
- *  3-4. 5 * 4 * 3 * D(2)
- *  3-5. 5 * 4 * 3 * 2 * D(1)
- *  3-6. 5 * 4 * 3 * 2 * 1
+ * 2. DFS에 N값 넘김
+ * 3. 1이나 2일때는 1 값 리턴
+ * 4. 그 외에는 N-2 + N-1 즉 N의 왼쪽 값 2개 더하기
+ *  4-1. 위에 내용으로 끝내도 코드는 돌아가지만 숫자가 커질 수록 너무 많은 양의 수를 재귀
+ *  4-2. N 번째 값이 이미 0보다 크다면 더 이상 볼 필요 없이 N 번째 값 리턴
+ * 5. 스택프레임 구조이므로 이전에 풀었던 for문과 속도차이가 많이 남
  */
-
 public class Java07_04 {
 
+    static int[] answer;
+
     public int DFS(int N) {
-        if (N == 1) return 1;
-        else return N * DFS(N - 1);
+        // 없어도 돌아가지만 속도에서 차이가 엄청나게 남
+        // 이미 구한 값이 존재하면 그 값을 가져오는거 이므로 "메모이제이션"이라고 불림
+        if (answer[N] > 0) return answer[N];
+
+        if (N == 1 || N == 2) return answer[N] = 1;
+        else return answer[N] = DFS(N - 2) + DFS(N - 1);
     }
+
 
     public static void main(String[] args) {
         Java07_04 T = new Java07_04();
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        answer = new int[N + 1];
 
-        System.out.println(T.DFS(N));
+        T.DFS(N);
+
+        for (int i = 1; i <= N; i++) System.out.print(answer[i]  + " ");
     }
 }

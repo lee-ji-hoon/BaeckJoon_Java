dbac387de0649f380c00943a0a2abae2158d472c
lee-ji-hoon <dlwlgns1240@naver.com>
2022-05-10 22:35:44 +0900

    백준 dp 2255 합분해

---
+++ src/baekjoon/dp/DP_2255.java
@@ -0,0 +1,23 @@
+package baekjoon.dp;
+
+import java.util.Scanner;
+public class DP_2255 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[][] dp = new int[201][201]; // N과 K 둘 다 최대 200까지
+
+        for (int i = 1; i <= K; i++) {
+            dp[i][0] = 1; // 기본 값들
+
+            for (int j = 1; j <= N; j++) {
+                // 1000000000 으로 미리 나눠서 dp에 저장해야지 overflow 방지 가능하다.
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1000000000;
+            }
+        }
+        System.out.println(dp[K][N]);
+    }
+}

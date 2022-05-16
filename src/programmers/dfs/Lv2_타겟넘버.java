f80e23c1752ad224f54ef40f857ab3045833f790
lee-ji-hoon <dlwlgns1240@naver.com>
2022-05-16 23:07:50 +0900

    프로그래머스 DFS Lv2 타겟넘버

---
+++ src/programmers/dfs/Lv2_타겟넘버.java
@@ -0,0 +1,18 @@
+package programmers.dfs;
+
+public class Lv2_타겟넘버 {
+    private static int answer;
+    public int solution(int[] numbers, int target) {
+        DFS(numbers, 0, target, 0);
+        return answer;
+    }
+
+    public void DFS(int[] numbers, int depth, int target, int sum){
+        if(depth == numbers.length) { // 마지막 노드까지 탐색했는지 확인
+            if(target == sum) answer ++; // 마지막 노드까지의 합이 target인지 확인
+        } else{
+            DFS(numbers, depth + 1, target, sum + numbers[depth]); // 더할 경우
+            DFS(numbers, depth + 1, target, sum - numbers[depth]); // 뺄 경우
+        }
+    }
+}

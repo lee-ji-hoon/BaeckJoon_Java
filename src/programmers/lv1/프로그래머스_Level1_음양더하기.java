a5bcc3d68c8caed5f1ec095a37bfc6733bbbbfb0
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-09 20:49:29 +0900

    프로그래머스_Level1_음양더하기

---
+++ src/programmers/lv1/프로그래머스_Level1_음양더하기.java
@@ -0,0 +1,10 @@
+package programmers.lv1;
+
+public class 프로그래머스_Level1_음양더하기 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for(int i=0; i < absolutes.length; i++) answer += absolutes[i] * (signs[i] ? 1 : -1);
+        return answer;
+    }
+}

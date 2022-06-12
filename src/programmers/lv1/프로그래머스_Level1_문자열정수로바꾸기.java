72d611d51a9c068088fd5c2e49bfac5646e2f7e6
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-12 19:48:10 +0900

    프로그래머스_Level1_문자열정수로바꾸기

---
+++ src/programmers/lv1/프로그래머스_Level1_문자열정수로바꾸기.java
@@ -0,0 +1,11 @@
+package programmers.lv1;
+
+class 프로그래머스_Level1_문자열정수로바꾸기 {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s.substring(0));
+
+        return answer;
+    }
+}

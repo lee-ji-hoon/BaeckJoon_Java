902ff0dc515c9a0f5277bc3abc30ead7bb92454e
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-19 15:37:44 +0900

    복습) 인프런 중복문자제거

---
+++ src/inflearn/review/Step1_6_중복문자제거.java
@@ -0,0 +1,23 @@
+package inflearn.review;
+
+import java.util.Scanner;
+
+public class Step1_6_중복문자제거 {
+    public static void main(String[] args) {
+        Step1_6_중복문자제거 T = new Step1_6_중복문자제거();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        System.out.println((T.solution(str)));
+    }
+
+    public String solution(String str) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (answer.toString().indexOf(str.charAt(i)) == -1)
+                answer.append(str.charAt(i));
+        }
+
+        return answer.toString();
+    }
+}

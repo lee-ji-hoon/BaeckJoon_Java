357f2c825caa3032bb89765fe5af24c423e30af3
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-19 14:09:13 +0900

    복습) 인프런 대소문자변환

---
+++ src/inflearn/review/Step1_2_대소문자변환.java
@@ -0,0 +1,23 @@
+package inflearn.review;
+
+import java.util.Scanner;
+
+public class Step1_2_대소문자변환 {
+    public String solution(String str){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            char find = str.charAt(i);
+            if('a' <= find && find <= 'z') sb.append((char)(find - 32));
+            else sb.append((char)(find + 32));
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+        Step1_2_대소문자변환 T = new Step1_2_대소문자변환();
+
+        Scanner in = new Scanner(System.in);
+        String alpha = in.next();
+
+        System.out.println(T.solution(alpha));
+    }
+}

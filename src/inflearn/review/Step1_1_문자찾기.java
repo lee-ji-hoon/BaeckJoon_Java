13e6220461d3856c391290e38c2ed1bef1c68d9b
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-19 14:00:46 +0900

    복습) 인프런 문자 찾기

---
+++ src/inflearn/review/Step1_1_문자찾기.java
@@ -0,0 +1,24 @@
+package inflearn.review;
+
+import java.util.Scanner;
+
+public class Step1_1_문자찾기 {
+    public int solution(String alpha, String find){
+        int answer = 0;
+        String upperAlpha = alpha.toUpperCase();
+        String upperFind = find.toUpperCase();
+
+        for(int i = 0; i < alpha.length(); i++){
+            if(String.valueOf(upperAlpha.charAt(i)).equals(upperFind)) answer++;
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Step1_1_문자찾기 T = new Step1_1_문자찾기();
+        Scanner in=new Scanner(System.in);
+
+        String alpha = in.next();
+        String find = in.next();
+        System.out.println(T.solution(alpha, find));
+    }
+}

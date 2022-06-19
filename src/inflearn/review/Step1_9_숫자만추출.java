37075f889e540913033565dfc993993aab184e3b
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-19 17:22:50 +0900

    복습) 인프런 숫자만추출

---
+++ src/inflearn/review/Step1_9_숫자만추출.java
@@ -0,0 +1,19 @@
+package inflearn.review;
+
+import java.util.Scanner;
+
+public class Step1_9_숫자만추출 {
+
+    public int solution(String str) {
+        return Integer.parseInt(str.replaceAll("[a-zA-Z]", ""));
+    }
+    public static void main(String[] args) {
+        Step1_9_숫자만추출 T = new Step1_9_숫자만추출();
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+
+    }
+}

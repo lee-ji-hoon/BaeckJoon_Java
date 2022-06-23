f5392fb1834c11cb173c16ca80d190dca7f02656
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-23 11:24:26 +0900

    프로그래머스_Level2_3차진수게임 다른풀이

---
+++ src/programmers/lv2/프로그래머스_Level2_3차진수게임_다른풀이_카카오.java
@@ -0,0 +1,32 @@
+package programmers.lv2;
+
+import java.util.Locale;
+
+class 프로그래머스_Level2_3차진수게임_다른풀이_카카오 {
+
+    public static String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+
+        int num = 0;
+        int length = m * t;
+        while (sb.length() < length)
+            sb.append(Integer.toString(num++, n));
+
+        for (int i = p - 1; i < length; i += m)
+            answer.append(sb.charAt(i));
+
+        return answer.toString().toUpperCase();
+    }
+
+
+    public static void main(String[] args) {
+        int n = 16; // 진법 n
+        int t = 16; // 구할 숫자의 갯수 t
+        int m = 2; // 게임에 참가하는 인원 m
+        int p = 1; // 튜브의 순서 p
+
+        String answer = solution(n, t, m, p);
+        System.out.println(answer);
+    }
+}

b5405610e346fd9f19ef6d389c5d27f92e3a155a
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-26 19:07:59 +0900

    중복 순열 추가

--- src/etc/Combination.java
+++ src/etc/Combination.java
@@ -1,6 +1,7 @@
 package etc;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Combination {
 
@@ -29,6 +30,40 @@ public class Combination {
         }
     }
 
+    //조합 (순서 관심 없고 뽑은 유무만 생각)
+    private static void combination(int[] comArr, int n, int r, int index, int target) {
+        if(r==0){
+            for(int i : comArr){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+            return;
+        }
+        if(target == n)return;
+
+        comArr[index] = target;
+        combination(comArr, n, r-1, index+1, target+1); //뽑는 경우
+        combination(comArr, n, r, index, target+1); //안 뽑는 경우
+    }
+
+    private static void permutaion(int[] arr, boolean[] visited, int n, int r, Stack<Integer> stack) {
+        if(stack.size() == r){
+            for (Integer integer : stack)
+                System.out.print(integer + " ");
+            System.out.println();
+        } else{
+            for (int i = 0; i < n; i++) {
+                if(!visited[i]){
+                    visited[i] = true;
+                    stack.push(arr[i]);
+                    permutaion(arr, visited, n, r, stack);
+                    stack.pop();
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
     private static void print(int[] arr, boolean[] visited, int n) {
         for (int i = 0; i < n; i++) {
             if (visited[i]) {
@@ -37,6 +72,8 @@ public class Combination {
         }
         System.out.println();
     }
+
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -50,6 +87,7 @@ public class Combination {
         for (int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
 
+
         System.out.println("몇 개의 조합으로 구현할지 입력해주세요");
         int r = sc.nextInt();
 
@@ -58,5 +96,10 @@ public class Combination {
         combination_backTracking(arr, visited, start,  n, r);
         System.out.println("========재귀 구현========");
         combination_recursive(arr, visited, start, n, r);
+        Stack<Integer> stack = new Stack<>();
+        System.out.println("========순열 구현========");
+        permutaion(arr, visited, n, r, stack);
     }
+
+
 }

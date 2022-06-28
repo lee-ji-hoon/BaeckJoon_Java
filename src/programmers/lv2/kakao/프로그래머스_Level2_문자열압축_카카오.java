007c42203c309e09ebb1ef512143e63f1b2d908c
lee-ji-hoon <dlwlgns1240@naver.com>
2022-06-28 20:08:15 +0900

    코드 수정

--- src/programmers/lv2/kakao/프로그래머스_Level2_문자열압축_카카오.java
+++ src/programmers/lv2/kakao/프로그래머스_Level2_문자열압축_카카오.java
@@ -12,11 +12,15 @@ class 프로그래머스_Level2_문자열압축_카카오 {
             String nextTarget = "";
 
             for (int start = i; start <= s.length(); start +=i){
-                if(start + i >= s.length()) nextTarget = s.substring(start, s.length());
-                else nextTarget = s.substring(start, start + i);
+                // 문자열 자르기
+                if(start + i >= s.length())
+                    nextTarget = s.substring(start, s.length());
+                else
+                    nextTarget = s.substring(start, start + i);
 
-
-                if(target.equals(nextTarget)) overlapCount++;
+                // 기준 문자열과 다음 문자열 비교
+                if(target.equals(nextTarget))
+                    overlapCount++;
                 else if(overlapCount == 1){
                     temp.append(target);
                     target = nextTarget;
@@ -26,15 +30,16 @@ class 프로그래머스_Level2_문자열압축_카카오 {
                     overlapCount = 1;
                 }
             }
-            if(i != target.length()) temp.append(target);
+
+            temp.append(target);
+
             answer = Math.min(answer, temp.length());
         }
-        System.out.println("answer = " + answer);
         return answer;
     }
 
     public static void main(String[] args) {
         프로그래머스_Level2_문자열압축_카카오 solution = new 프로그래머스_Level2_문자열압축_카카오();
-        solution.solution("ababcdcdababcdcd");
+        solution.solution("xababcdcdababcdcd");
     }
 }
